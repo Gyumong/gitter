9085385b3921ca87424c5cac154e0f411c943d38
Gyumong <rlaalsrb3559@naver.com>
2020-11-26 18:03:41 +0900

    트윗 생성, 트윗 불러오기 기능 완성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { dbService } from "../fbserver";
 
 function Home() {
   const [gweet, setGweet] = useState("");
+  const [dbGweets, setdbGweets] = useState([]);
+
+  const getGweets = async () => {
+    const gweets = await dbService.collection("gweets").get();
+    gweets.forEach((docs) => {
+      const gweetObject = {
+        ...docs.data(),
+        id: docs.id,
+      };
+      setdbGweets((prev) => [gweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getGweets();
+  }, []);
 
   const onChange = (e) => {
     setGweet(e.target.value);
   };
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await dbService.collection("gweets").add({
+      gweet,
+    });
+    setGweet("");
   };
+  console.log(dbGweets);
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           value={gweet}
@@ -19,8 +40,15 @@ function Home() {
           placeholder="지금 무슨 생각해?"
           maxLength={120}
         />
-        <input type="submit" value="Gweet" onSubmit={onSubmit} />
+        <input type="submit" value="Gweet" />
       </form>
+      <div>
+        {dbGweets.map((gweet) => (
+          <div key={gweet.id}>
+            <h4>{gweet.gweet}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
